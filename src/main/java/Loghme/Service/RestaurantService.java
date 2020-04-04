84d2f956f33456965c3a414b016955656106f8c9
Mir Hamed <h.jafar77@gmail.com>
2020-04-04 23:02:55 +0430

    Error handling for Restaurants completed.

--- src/main/java/Loghme/Service/RestaurantService.java
+++ src/main/java/Loghme/Service/RestaurantService.java
@@ -3,8 +3,9 @@ package Loghme.Service;
 import Loghme.Exceptions.Error403;
 import Loghme.Exceptions.Error404;
 import Loghme.Model.CommandHandler;
-import Loghme.Model.Restaurant;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -13,7 +14,13 @@ import java.io.IOException;
 public class RestaurantService {
 
     @RequestMapping(value = "/restaurant/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Restaurant getRestaurant(@PathVariable(value = "id") String id) throws IOException, Error404, Error403 {
-        return CommandHandler.getInstance().getRestaurant(id);
+    public ResponseEntity<?> getRestaurant(@PathVariable(value = "id") String id) {
+        try {
+            return new ResponseEntity<>(CommandHandler.getInstance().getRestaurant(id), HttpStatus.OK);
+        } catch (Error404 | IOException error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Error403 error403) {
+            return new ResponseEntity<>(error403.getMessage(), HttpStatus.FORBIDDEN);
+        }
     }
 }
--- src/main/java/Loghme/Service/RestaurantsService.java
+++ src/main/java/Loghme/Service/RestaurantsService.java
@@ -2,20 +2,22 @@ package Loghme.Service;
 
 import Loghme.Exceptions.Error404;
 import Loghme.Model.CommandHandler;
-import Loghme.Model.Restaurant;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 @RestController
 public class RestaurantsService {
 
     @RequestMapping(value = "/restaurants", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ArrayList<Restaurant> getRestaurants() throws IOException, Error404 {
-        return CommandHandler.getInstance().getRestaurants();
+    public ResponseEntity<?> getRestaurants() {
+        try {
+            return new ResponseEntity<>(CommandHandler.getInstance().getRestaurants(), HttpStatus.OK);
+        } catch (Error404 | IOException error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
