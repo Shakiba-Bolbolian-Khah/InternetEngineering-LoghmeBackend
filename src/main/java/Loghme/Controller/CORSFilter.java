e71c384ae0415802e4ea073b3f2fd2a22e8b1cef
Mir Hamed <h.jafar77@gmail.com>
2020-04-12 16:11:33 +0430

    Server delay added.

--- src/main/java/Loghme/Controller/CORSFilter.java
+++ src/main/java/Loghme/Controller/CORSFilter.java
@@ -20,19 +20,26 @@ public class CORSFilter implements Filter {
 
         public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 
-            // Authorize (allow) all domains to consume the content
-            ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Origin","*");
-            ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Methods","OPTIONS, GET, POST, PUT, DELETE, HEAD");
-            ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, X-Device-User-Agent, Content-Type, Accept, Authentication");
+            // Server delay
+            try {
+                Thread.sleep(3000);
 
-            // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
-            if (((HttpServletRequest) servletRequest).getMethod().equals("OPTIONS")) {
-                ((HttpServletResponse) servletResponse).setStatus(HttpServletResponse.SC_ACCEPTED);
-                return;
+                // Authorize (allow) all domains to consume the content
+                ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Origin","*");
+                ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Methods","OPTIONS, GET, POST, PUT, DELETE, HEAD");
+                ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, X-Device-User-Agent, Content-Type, Accept, Authentication");
+
+                // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
+                if (((HttpServletRequest) servletRequest).getMethod().equals("OPTIONS")) {
+                    ((HttpServletResponse) servletResponse).setStatus(HttpServletResponse.SC_ACCEPTED);
+                    return;
+                }
+
+                // pass the request along the filter chain
+                chain.doFilter(servletRequest, servletResponse);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
-            // pass the request along the filter chain
-            chain.doFilter(servletRequest, servletResponse);
         }
 
         public void init(FilterConfig fConfig) throws ServletException {
